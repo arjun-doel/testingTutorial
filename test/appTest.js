7ef02553fb8b58128888407a72e63888883667b1
Arjun Doel <arjundoel@gmail.com>
2021-09-14 15:42:43 +0100

    result clean up

--- test/appTest.js
+++ test/appTest.js
@@ -1,26 +1,25 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+// Results
+sayHelloResult = app.sayHello()
+addNumbersResult = app.addNumbers(5,5)
 
 describe('App', function(){
   it('app should return hello', function(){
-    let result = sayHello()
-    assert.equal(result, 'hello');
+    assert.equal(sayHelloResult, 'hello');
   });
 
   it('sayHello should return type string', function() {
-    let result = sayHello();
-    assert.typeOf(result, 'string');
+    assert.typeOf(sayHelloResult, 'string');
   });
 
   it('addNumbers should be above 5', function(){
-    let result = addNumbers(5,5);
-    assert.isAbove(result, 5);
-  })
+    assert.isAbove(addNumbersResult, 5);
+  });
 
   it('addNumbers should return type number', function() {
-    let result = addNumbers();
-    assert.typeOf(result, 'Number');
+    assert.typeOf(addNumbersResult, 'Number');
   });
 
 });
