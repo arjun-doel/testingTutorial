2e41a17e9e1496a15c2793042ea283748295bbbd
Arjun Doel <arjundoel@gmail.com>
2021-09-14 15:49:58 +0100

    palindrome func

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ module.exports = {
   },
   addNumbers: function(value1, value2) {
     return value1 + value2
+  },
+  palindrome: function(string){
+    const array = string.split('')
+    const reverse = array.reverse()
+    return reverse.join('') === string
   }
 }
--- test/appTest.js
+++ test/appTest.js
@@ -4,6 +4,9 @@ const app = require('../app');
 // Results
 sayHelloResult = app.sayHello()
 addNumbersResult = app.addNumbers(5,5)
+palindromeResult1 = app.palindrome('dad')
+palindromeResult2 = app.palindrome('mum')
+palindromeResult3 = app.palindrome('tree')
 
 describe('App', function(){
   describe('sayHello()', function(){
@@ -25,5 +28,20 @@ describe('App', function(){
       assert.typeOf(addNumbersResult, 'Number');
     });
   });
+
+  describe('palindrome', function(){
+    it('should return a true', function(){
+      assert.equal(palindromeResult1, true)
+    });
+
+    it('should return a true', function(){
+      assert.equal(palindromeResult2, true)
+    });
+
+    it('should return a false', function(){
+      assert.equal(palindromeResult3, false)
+    });
+
+  })
 
 });
