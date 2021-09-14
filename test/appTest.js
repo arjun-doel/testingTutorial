bd0e56f960759fbdafdde23ac953bdd4df3c9592
Arjun Doel <arjundoel@gmail.com>
2021-09-14 15:39:59 +0100

    number func

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,26 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 
 describe('App', function(){
   it('app should return hello', function(){
-    assert.equal(sayHello(), 'hello');
+    let result = sayHello()
+    assert.equal(result, 'hello');
   });
+
+  it('sayHello should return type string', function() {
+    let result = sayHello();
+    assert.typeOf(result, 'string');
+  });
+
+  it('addNumbers should be above 5', function(){
+    let result = addNumbers(5,5);
+    assert.isAbove(result, 5);
+  })
+
+  it('addNumbers should return type number', function() {
+    let result = addNumbers();
+    assert.typeOf(result, 'Number');
+  });
+
 });
